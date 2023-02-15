17bb35ce306ef1153fca21790b14f5a024d1d3f6
Oleksii Hlembytskyi <OLHI@simcorp.com>
2023-02-15 15:00:02 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestCalculator
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 10;
+            int b = 20;
+
+            Calculator calculator = new Calculator();
+            int res = calculator.Sum(a, b);
+
+            Assert.AreEqual(res, 30);
+        }
+    }
+}
